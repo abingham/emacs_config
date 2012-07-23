0a9f96b4db4d855ffe96d74a491057cbb92202d4
Austin Bingham <austin.bingham@roxar.com>
2012-07-23 12:29:49 +0200

    Updated yasnippet config.

--- yasnippet/config.el
+++ yasnippet/config.el
@@ -1,8 +1,8 @@
 (require 'yasnippet)
 
-(setq yas/root-directory (concat (file-name-directory load-file-name) "/snippets"))
-(yas/load-directory yas/root-directory)
+(yas/initialize)
+(yas/load-directory (concat (file-name-directory load-file-name) "/snippets"))
 
-(setq yas/prompt-functions '(yas/ido-prompt yas/dropdown-prompt))
+(setq yas/prompt-functions '(yas/completing-prompt yas/x-prompt))
 
 (global-set-key "\C-c\C-y" 'yas/insert-snippet)
