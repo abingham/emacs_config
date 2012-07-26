ee6aa1d1f7ea18bb1618a85ef1766e1efb549145
Austin Bingham <austin.bingham@roxar.com>
2012-07-26 11:07:07 +0200

    fixed up autocomplete config.

--- autocomplete/config.el
+++ autocomplete/config.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path (concat (file-name-directory load-file-name) "/auto-complete-1.3.1"))
-
 (require 'auto-complete)
+
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
+
+(require 'auto-complete-config)
+(ac-config-default)
 (global-auto-complete-mode t)
-; (define-key ac-completing-map (kbd "M-TAB") 'ac-complete)
+;(define-key ac-completing-map (kbd "M-TAB") 'ac-complete)
