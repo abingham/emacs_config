507bc51081e81d822b0238b6ab8e96fd0c433ff2
Austin Bingham <austin.bingham@gmail.com>
2012-11-18 21:54:25 +0100

    moved keybindings to end of config

--- config.el
+++ config.el
@@ -8,59 +8,6 @@
 	      (file-name-directory load-file-name) 
 	      "/elisp"))
 
-;; Key bindings
-(global-set-key [(ctrl x) (i)]    'windmove-up)
-(global-set-key [(ctrl x) (m)]  'windmove-down)
-(global-set-key [(ctrl x) (j)]  'windmove-left)
-(global-set-key [(ctrl x) (l)] 'windmove-right)
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-w" 'backward-kill-word) 
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-x\C-j" 'copy-region-as-kill)
-(global-set-key "\C-x\C-n" 'compile)
-(defun up-slightly () (interactive) (scroll-up 5))
-(defun down-slightly () (interactive) (scroll-down 5))
-(global-set-key (kbd "<mouse-5>") 'up-slightly)
-(global-set-key (kbd "<mouse-4>") 'down-slightly)
-(global-set-key "\C-l" 'line-to-top-of-window)
-(global-set-key [f9] 'match-paren)
-(global-set-key [f10] 'start-kbd-macro)
-(global-set-key [f11] 'end-kbd-macro)
-(global-set-key [f12] 'call-last-kbd-macro)
-
-
-;; display and behavior
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(global-font-lock-mode 1)
-(setq inhibit-splash-screen 't)
-(global-auto-revert-mode 't)
-(show-paren-mode 1)
-(column-number-mode 1)
-(line-number-mode 1)
-(setq visible-bell t)
-
-;; Compilation stuff
-(setq-default display-buffer-reuse-frames t)
-(setq compile-command "scons -u")
-(setq compilation-scroll-output 't)
-(defun my-compilation-mode-hook ()
-  (define-key compilation-mode-map "\C-x\C-n" 'next-error)
-  (define-key compilation-mode-map "\C-x\C-p" 'previous-error)
-  )
-(add-hook 'compilation-mode-hook 'my-compilation-mode-hook)
-
-;; Misc. other stuff
-(setq c-basic-offset 2)
-(require 'cl)
-(require 'rtf-mode)
-(require 'open-next-line)
-(require 'tramp)
-(setq tramp-default-method "ssh")
-
-(fset 'yes-or-no-p 'y-or-n-p)           ; easy answer to stupid question
-
 ;; --- mode configurations and such ---
 (setq config_packages
       '("anything"
@@ -138,6 +85,59 @@ Replaces three keystroke sequence C-u 0 C-l."
         (narrow-to-region start end)
         (goto-char (point-min))
         (message (number-to-string (count-matches "\\sw+"))))))
+
+;; Key bindings
+(global-set-key [(ctrl x) (i)]    'windmove-up)
+(global-set-key [(ctrl x) (m)]  'windmove-down)
+(global-set-key [(ctrl x) (j)]  'windmove-left)
+(global-set-key [(ctrl x) (l)] 'windmove-right)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word) 
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-x\C-j" 'copy-region-as-kill)
+(global-set-key "\C-x\C-n" 'compile)
+(defun up-slightly () (interactive) (scroll-up 5))
+(defun down-slightly () (interactive) (scroll-down 5))
+(global-set-key (kbd "<mouse-5>") 'up-slightly)
+(global-set-key (kbd "<mouse-4>") 'down-slightly)
+(global-set-key "\C-l" 'line-to-top-of-window)
+(global-set-key [f9] 'match-paren)
+(global-set-key [f10] 'start-kbd-macro)
+(global-set-key [f11] 'end-kbd-macro)
+(global-set-key [f12] 'call-last-kbd-macro)
+
+
+;; display and behavior
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(global-font-lock-mode 1)
+(setq inhibit-splash-screen 't)
+(global-auto-revert-mode 't)
+(show-paren-mode 1)
+(column-number-mode 1)
+(line-number-mode 1)
+(setq visible-bell t)
+
+;; Compilation stuff
+(setq-default display-buffer-reuse-frames t)
+(setq compile-command "scons -u")
+(setq compilation-scroll-output 't)
+(defun my-compilation-mode-hook ()
+  (define-key compilation-mode-map "\C-x\C-n" 'next-error)
+  (define-key compilation-mode-map "\C-x\C-p" 'previous-error)
+  )
+(add-hook 'compilation-mode-hook 'my-compilation-mode-hook)
+
+;; Misc. other stuff
+(setq c-basic-offset 2)
+(require 'cl)
+(require 'rtf-mode)
+(require 'open-next-line)
+(require 'tramp)
+(setq tramp-default-method "ssh")
+
+(fset 'yes-or-no-p 'y-or-n-p)           ; easy answer to stupid question
 
 ; Load any local settings.
 (let* ((this_dir (file-name-directory load-file-name))
