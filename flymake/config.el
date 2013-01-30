6c1124c3dc51435ecb0d4bbbcd49c9f87d461e81
Austin Bingham <austin.bingham@emerson.com>
2013-01-30 09:16:49 +0100

    Making flymake work with python.

--- flymake/config.el
+++ flymake/config.el
@@ -1,38 +1,26 @@
 (require 'flymake)
 
-(defvar flymake-additional-compilation-flags nil)
-(put 'flymake-additional-compilation-flags 'safe-local-variable 'listp)
+;; (defvar flymake-additional-compilation-flags nil)
+;; (put 'flymake-additional-compilation-flags 'safe-local-variable 'listp)
  
-;; no need to arrange Makefile
-(defun flymake-cc-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name)))
-         (common-args (append (list "-Wall" "-W" "-fsyntax-only" local-file)
-                              flymake-additional-compilation-flags)))
-    (if (eq major-mode 'c++-mode)
-        (list "g++" common-args)
-      (list "g++" common-args))))
+;; ;; no need to arrange Makefile
+;; (defun flymake-cc-init ()
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                      'flymake-create-temp-inplace))
+;;          (local-file (file-relative-name
+;;                       temp-file
+;;                       (file-name-directory buffer-file-name)))
+;;          (common-args (append (list "-Wall" "-W" "-fsyntax-only" local-file)
+;;                               flymake-additional-compilation-flags)))
+;;     (if (eq major-mode 'c++-mode)
+;;         (list "g++" common-args)
+;;       (list "g++" common-args))))
  
-(loop for ext in '("\\.c$" "\\.h$" "\\.cc$" "\\.cpp$" "\\.hh$" "\\.hpp$")
-      do
-      (push `(,ext flymake-cc-init) flymake-allowed-file-name-masks))
+;; (loop for ext in '("\\.c$" "\\.h$" "\\.cc$" "\\.cpp$" "\\.hh$" "\\.hpp$")
+;;       do
+;;       (push `(,ext flymake-cc-init) flymake-allowed-file-name-masks))
  
-(defun flymake-pylint-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "pyflakes3" (list local-file))))
-
-(add-to-list 'flymake-allowed-file-name-masks
-             '("\\.py\\'" flymake-pylint-init))
-
-(add-hook 'c-mode-hook (lambda () (flymake-mode t)))
-(add-hook 'c++-mode-hook (lambda () (flymake-mode t)))
-(add-hook 'python-mode-hook (lambda () (flymake-mode t)))
+;;(add-hook 'c-mode-hook (lambda () (flymake-mode t)))
+;;(add-hook 'c++-mode-hook (lambda () (flymake-mode t)))
 
 (global-set-key [(ctrl c) (f)] 'flymake-display-err-menu-for-current-line)
--- python/config.el
+++ python/config.el
@@ -1,120 +1,13 @@
-; mode-specific stuff
-(add-to-list 'load-path (concat (file-name-directory load-file-name) "/gallina"))
-;(setq interpreter-mode-alist (cons '("python" . python-mode)
-;                                   interpreter-mode-alist))
-;(autoload 'python-mode "python-mode" "Python editing mode." t)
 (require 'python)
+(require 'python-pep8)
+(require 'flymake-python-pyflakes)
 
-; mode-independent stuff
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\SConscript\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\SConstruct\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\wscript\\'" . python-mode))
 (setq python-indent-offset 4)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; lambda mode
-(autoload 'lambda-mode "lambda-mode")
-(add-hook 'python-mode-hook #'lambda-mode 1)
-(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
-
-
-;(require 'anything)
-; (require 'anything-ipython)
-;(when (require 'anything-show-completion nil t)
-;  (use-anything-show-completion 'anything-ipython-complete
-;                                '(length initial-pattern)))
-
-;; (add-hook 'python-mode-hook
-;;           #'(lambda ()
-;;               (define-key py-mode-map (kbd "M-<tab>") 'ipython-complete)))
-;; (add-hook 'ipython-shell-hook
-;;           #'(lambda ()
-;;               (define-key py-mode-map (kbd "M-<tab>") 'ipython-complete)))
-
-
-;(require 'comint)
-;(define-key comint-mode-map (kbd "M-") 'comint-next-input)
-;(define-key comint-mode-map (kbd "M-") 'comint-previous-input)
-;(define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
-;(define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pylookup stuff
-; Create new database: ./pylookup.py -u file:///usr/share/doc/python2.6-doc/html
-;; add pylookup to your loadpath, ex) "~/.lisp/addons/pylookup"
-(setq pylookup-dir "~/.emacs.d/python/pylookup")
-(add-to-list 'load-path pylookup-dir)
-;; load pylookup when compile time
-(eval-when-compile (require 'pylookup))
-
-;; set executable file and db file
-(setq pylookup-program (concat pylookup-dir "/pylookup.py"))
-(setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
-
-;; to speedup, just load it on demand
-(autoload 'pylookup-lookup "pylookup"
-  "Lookup SEARCH-TERM in the Python HTML indexes." t)
-(autoload 'pylookup-update "pylookup"
-  "Run pylookup-update and create the database at `pylookup-db-file'." t)
-(global-set-key "\C-cp" 'pylookup-lookup)
-
-
-(require 'python-pep8)
-(autoload 'python-pylint "python-pylint")
-(autoload 'pylint "python-pylint")
-
-
-                                     ; this get called after python mode is enabled
-(defun my-python-hook ()
-                                     ; outline uses this regexp to find headers. I match lines with no indent and indented "class"
-                                     ; and "def" lines.
-  ;(setq outline-regexp "[^ \t]\\|[ \t]*\\(def\\|class\\) ")
-  ;(setq outline-regexp " *\\(def\\|class\\) ")
-                                     ; enable our level computation
-  ;(setq outline-level 'py-outline-level)
-                                     ; do not use their \C-c@ prefix, too hard to type. Note this overides some python mode bindings
-  ;(setq outline-minor-mode-prefix "\C-c")
-                                     ; turn on outline mode
-  ;(outline-minor-mode t)
-                                     ; initially hide all but the headers
-  ;(hide-body)
-                                     ; I use CUA mode on the PC so I rebind these to make the more accessible
-  ;(local-set-key [?\C-\t] 'py-shift-region-right)
-  ;(local-set-key [?\C-\S-\t] 'py-shift-region-left)
-  ;(local-set-key "\M-f" 'forward-word)
-  ;(local-set-key "\M-b" 'backward-word)
-                                     ; make paren matches visible
-  (show-paren-mode 1)
-  (highlight-indentation-mode 1)
-  )
-
-                                        ; add my customization
-(add-hook 'python-mode-hook 'my-python-hook)
-
-; pymacs
-;(autoload 'pymacs-apply "pymacs")
-;(autoload 'pymacs-call "pymacs")
-;(autoload 'pymacs-eval "pymacs" nil t)
-;(autoload 'pymacs-exec "pymacs" nil t)
-;(autoload 'pymacs-load "pymacs" nil t)
-;(autoload 'pymacs-autoload "pymacs")
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
-
-;; ropemacs
-;(setq ropemacs-enable-shortcuts nil)
-;(setq ropemacs-local-prefix "C-c C-p")
-;(pymacs-load "ropemacs" "rope-")
-;(setq ropemacs-enable-autoimport t)
-
-;; -- one way to lazy-load ropemacs
-;(defun load-ropemacs ()
-;  "Load pymacs and ropemacs"
-;  (interactive)
-;  (require 'pymacs)
-;  (pymacs-load "ropemacs" "rope-")
-;  ;; Automatically save project python buffers before refactorings
-;  (setq ropemacs-confirm-saving 'nil)
-;  )
-; (global-set-key "\C-xpl" 'load-ropemacs)
+(setq flymake-python-pyflakes-executable "/usr/local/bin/pyflakes3")
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+(add-hook 'python-mode-hook (lambda () (show-paren-mode t)))
