0195cb5935e8e389f7ddb2e061cdd6bf017fffa0
Austin Bingham <austin.bingham@emerson.com>
2013-01-30 16:26:03 +0100

    C++ flymake dorking around.

--- flymake/config.el
+++ flymake/config.el
@@ -1,2 +1,7 @@
 (require 'flymake)
+
 (global-set-key [(ctrl c) (f)] 'flymake-display-err-menu-for-current-line)
+
+;(custom-set-variables
+;     '(help-at-pt-timer-delay 0.9)
+;     '(help-at-pt-display-when-idle '(flymake-overlay)))
