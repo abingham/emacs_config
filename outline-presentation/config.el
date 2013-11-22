d48872090cd4e94e6c6a2a5e60f2098c33ad3f9b
Austin Bingham <austin.bingham@gmail.com>
2013-11-22 10:35:43 +0100

    Added outline-presentation-mode stuff

---
+++ outline-presentation/config.el
@@ -0,0 +1,3 @@
+(require 'outline-presentation)
+(global-set-key [(ctrl c) (.)] 'outline-presentation-next)
+(global-set-key [(ctrl c) (\,)] 'outline-presentation-previous)
---
+++ outline-presentation/outline-presentation-bindings.el
@@ -0,0 +1,5 @@
+; Experimental keybindings for outline-presentation-mode
+(global-set-key [(ctrl c) (.)] 'outline-presentation-next)
+(global-set-key [(ctrl c) (,)] 'outline-presentation-previous)
+
+
