ba601398fc265c665a02b3b9820d867c43a99e03
Austin Bingham <austin.bingham@emerson.com>
2013-11-25 08:36:36 +0100

    helm: reverted to old execute-extended-command

--- helm/config.el
+++ helm/config.el
@@ -8,13 +8,9 @@
 (global-set-key [(ctrl h) (f)] 'helm-apropos)
 (global-set-key [(ctrl h) (v)] 'helm-apropos)
 
-;; (helm :sources '(helm-source-findutils
-;;                  helm-source-recentf
-;;                  helm-source-bookmarks
-;;                  helm-source-buffers-list
-;;                  helm-source-locate
-;;                  helm-source-ls-git)
-;;       :buffer "*helm all the things*")
-; (global-set-key "\C-u" (helm))
+; Leave execute-extended-command alone. I like the normal behavior for now.
+(add-to-list 'helm-completing-read-handlers-alist
+             '(execute-extended-command . nil))
+
 
 
