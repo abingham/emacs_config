04fa69a5b7c555627a7f589d600a3b4be3ff8259
Austin Bingham <austin.bingham@emerson.com>
2013-12-02 09:42:34 +0100

    added ace-jump mode

---
+++ modules/ace_jump/config.el
@@ -0,0 +1,4 @@
+(require 'ace-jump-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-c C-u SPC") 'ace-jump-char-mode)
+(define-key global-map (kbd "C-c C-u C-u SPC") 'ace-jump-line-mode)
