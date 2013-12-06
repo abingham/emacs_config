e3bf3de0922d65b110a543a55ba7c428e62fcf9d
Austin Bingham <austin.bingham@gmail.com>
2013-12-06 08:37:24 +0100

    Updated helm source order

--- modules/misc/config.el
+++ modules/misc/config.el
@@ -1,10 +1,10 @@
 (defun helm-my-buffers ()
   (interactive)
-  (helm-other-buffer '(
+  (helm-other-buffer '(helm-c-source-prosjekt-projects
+		       helm-c-source-prosjekt-files
+		       helm-source-buffers-list
                        helm-source-files-in-current-dir
                        helm-source-recentf
-                       helm-c-source-prosjekt-projects
-                       helm-c-source-prosjekt-files
                        helm-source-buffer-not-found)
                      "*helm-my-buffers*"))
 (global-set-key "\C-u" 'helm-my-buffers)
