f3b5d42f811334b74e9963ae99535d06a59af465
Austin Bingham <austin.bingham@emerson.com>
2014-01-22 10:42:20 +0100

    Using any available port for traad servers.

--- modules/traad/config.el
+++ modules/traad/config.el
@@ -8,14 +8,14 @@
 
 (defun activate-traad3 ()
   (interactive)
-  (set-variable 'traad-server-port 9752)
+  (set-variable 'traad-server-port 0)
   (set-variable 'traad-server-args '("-V" "2"))
   (set-variable 'python-environment-root "~/.emacs.d/modules/traad/traad/venv3")
   (set-variable 'traad-server-program (python-environment-bin "traad3")))
 
 (defun activate-traad2 ()
   (interactive)
-  (set-variable 'traad-server-port 9752)
+  (set-variable 'traad-server-port 0)
   (set-variable 'traad-server-args '("-V" "2"))
   (set-variable 'python-environment-root "~/.emacs.d/modules/traad/traad/venv2")
   (set-variable 'traad-server-program (python-environment-bin "traad")))
