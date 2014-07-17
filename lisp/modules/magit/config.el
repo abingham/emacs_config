09630b46905e2e40cec132563e63d05dc4630842
Austin Bingham <austin.bingham@gmail.com>
2014-07-17 13:57:49 +0200

    Fixed magit config so we can edit commit messages in same session.

--- lisp/modules/magit/config.el
+++ lisp/modules/magit/config.el
@@ -1,2 +1,6 @@
+; This lets us edit commit messages in the same emacs session
+(set-variable 'magit-emacsclient-executable "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
+
 (require 'magit)
+
 (global-set-key [f5] 'magit-status)
