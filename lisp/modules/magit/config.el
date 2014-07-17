a391046fbbfdc4c64d61a2aff2e31005f5241bb1
Austin Bingham <austin.bingham@gmail.com>
2014-07-17 14:13:51 +0200

    Removed some cruft from magit config.

--- lisp/modules/magit/config.el
+++ lisp/modules/magit/config.el
@@ -1,6 +1,3 @@
-; This lets us edit commit messages in the same emacs session
-(set-variable 'magit-emacsclient-executable "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
-
 (require 'magit)
 
 (global-set-key [f5] 'magit-status)
