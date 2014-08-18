e2f5434b2872ff041b5ee1ba23b9ad4f2c919c70
Austin Bingham <austin.bingham@gmail.com>
2014-08-18 10:23:13 +0200

    Added prosjekt-grin command.

--- kfg/modules/prosjekt/config.el
+++ kfg/modules/prosjekt/config.el
@@ -9,9 +9,18 @@
 
 (require 'helm-prosjekt)
 
+(defun prosjekt-grin (pattern file-pattern)
+  (interactive
+   (list (read-string "pattern: ")
+         (read-string "file-pattern: ")))
+  (grep (concat "grin --emacs -I \"" file-pattern "\" " pattern " " prosjekt-proj-dir)))
+
 (global-set-key [(ctrl x) (p) (s)] 'prosjekt-setup)
 (global-set-key [(ctrl x) (p) (c)] 'prosjekt-close)
 (global-set-key [(ctrl x) (p) (r)] 'prosjekt-repopulate)
+(global-set-key [(ctrl x) (p) (g)] 'prosjekt-grin)
+
+
 
 (global-set-key 
  [f2] 
