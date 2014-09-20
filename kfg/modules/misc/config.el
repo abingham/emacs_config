495c752cd6ba53c7abc5304548cdddd88d65e7b2
Austin Bingham <austin.bingham@gmail.com>
2014-09-20 10:54:35 +0100

    Improved auto-revert-mode stuff.

--- kfg/modules/misc/config.el
+++ kfg/modules/misc/config.el
@@ -42,8 +42,9 @@
 (menu-bar-mode -1)
 (global-font-lock-mode 1)
 (setq inhibit-splash-screen 't)
-(global-auto-revert-mode 't)
-(setq auto-revert-interval 1)
+(custom-set-variables
+ '(auto-revert-interval 1))
+(global-auto-revert-mode 1)
 (show-paren-mode 1)
 (column-number-mode 1)
 (line-number-mode 1)
