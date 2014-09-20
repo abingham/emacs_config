f0db13310131275307cd033f3d2bd81241ebbdd9
Austin Bingham <austin.bingham@gmail.com>
2014-09-20 10:54:46 +0100

    helm-sources work properly when no project is open.

--- kfg/modules/helm/config.el
+++ kfg/modules/helm/config.el
@@ -14,12 +14,16 @@
 
 (defun helm-my-buffers ()
   (interactive)
-  (helm-other-buffer '(helm-source-projectile-files-list
-		       helm-source-projectile-buffers-list
-		       helm-source-projectile-projects
-		       helm-source-buffers-list
-                       helm-source-files-in-current-dir
-                       helm-source-recentf
-                       helm-source-buffer-not-found)
-                     "*helm-my-buffers*"))
+  (let* ((sources '(helm-source-projectile-projects
+		    helm-source-buffers-list
+		    helm-source-files-in-current-dir
+		    helm-source-recentf
+		    helm-source-buffer-not-found))
+	 (sources (if (projectile-project-p)
+		      (append '(helm-source-projectile-files-list
+				helm-source-projectile-buffers-list)
+			      sources)
+		    sources)))
+    (helm-other-buffer sources "*helm-my-buffers*")))
+
 (global-set-key "\C-u" 'helm-my-buffers)
