798d321b2101dde8410943dfbb2e15155605343a
Austin Bingham <austin.bingham@gmail.com>
2014-10-18 15:18:31 +0200

    Updated codesearch config to use csearch_excludes.

--- kfg/modules/codesearch/config.el
+++ kfg/modules/codesearch/config.el
@@ -1,4 +1,16 @@
 (require 'codesearch)
 
 (global-set-key "\M-'" 'codesearch-search)
-(global-set-key "\M-;" 'codesearch-search-at-point)
+
+(set-variable 'codesearch-cindex-flags '("-exclude" "~/.csearch_excludes"))
+
+(defun projectile-codesearch-search (pattern file-pattern)
+  (interactive
+   (list
+    (read-string "Pattern: " (thing-at-point 'symbol))
+    (read-string "File pattern: " ".*")))
+  (unless (projectile-project-root) (error "Not in a projectile project."))
+  (let ((fpatt (concat (projectile-project-root) file-pattern)))
+    (codesearch-search pattern fpatt)))
+
+(global-set-key "\M-." 'projectile-codesearch-search)
