7871e0a1c4e1292ac5e5cc2d7b7e5335a37d1496
Austin Bingham <austin.bingham@gmail.com>
2014-11-11 12:11:52 +0100

    Added helper functions for doing custom keymaps in projectile.

--- kfg/modules/projectile/config.el
+++ kfg/modules/projectile/config.el
@@ -1,3 +1,18 @@
+(require 'dash)
+
 (projectile-global-mode)
+
+(defun projectile-set-local-keymap (&rest bindings)
+  (dolist (binding (-partition-in-steps 2 2 bindings))
+    (lexical-let* ((key (car binding))
+                   (cmd (cadr binding))
+                   (is-interactive (interactive-form cmd))
+                   (local-map (or (current-local-map) (make-keymap))))
+      (define-key local-map key
+        (lambda ()
+          (interactive)
+          (if is-interactive
+              (call-interactively cmd)
+            (eval cmd)))))))
 
 (global-set-key [f6] 'projectile-run-shell-command-in-root)
