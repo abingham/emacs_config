1409536bbde9754f90b60cce9b3f662bb1d8432c
Austin Bingham <austin.bingham@gmail.com>
2014-12-09 08:12:17 +0100

    No more jumping to clojure error window.

--- kfg/modules/clojure/config.el
+++ kfg/modules/clojure/config.el
@@ -1,3 +1,5 @@
+(set-variable 'cider-auto-select-error-buffer nil)
+
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 ; (add-hook 'clojure-mode-hook 'ac-nrepl-setup)
