e4c4e4b528aad09880ff191ea90955fd5b8ff20d
Austin Bingham <austin.bingham@gmail.com>
2015-01-09 08:18:19 +0100

    Configuring paredit at init, not idle.

--- init.el
+++ init.el
@@ -263,7 +263,7 @@
 
 (use-package paredit
   :ensure t
-  :idle
+  :init
   (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
