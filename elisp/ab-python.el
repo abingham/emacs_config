f33df84f2db34a53aee9e2d9a32b8ca4bd87812e
Austin Bingham <austin.bingham@gmail.com>
2015-01-17 12:46:56 +0100

    Improved Python interpreter management.

--- elisp/ab-python.el
+++ elisp/ab-python.el
@@ -1,96 +1,171 @@
+;;; ab-python --- my python configuration
+;;; Commentary:
+
+;; We have some "global" things that are independent of venv:
+;;  - traad
+;;  - pyflakes
+;;  - ...maybe more...
+;;
+;; But these *are* dependent on the major version of Python. When we
+;; initially start the config should specify Python 2 or Python
+;; 3. This way we know which stuff to use in the absence of a
+;; virtualenv. When a virtualenv is selected, we should determine the
+;; major version and activate the right tools.
+;;
+;;
+
+;;; Code:
+
 (require 'dash)
 
-;; python-version specific stuff
-(defun activate-python2 ()
-  (interactive)
-  (setq python-shell-interpreter "ipython"
-        ; jedi:server-command (list "python2.7" jedi:server-script)
-        )
-  ;; (set-variable 'traad-server-program '("/usr/local/bin/traad"))
+(defun ab-python-major-version ()
+  "Find major version of current Python interpreter."
+  (substring
+   (shell-command-to-string
+    (format "%s -c \"import sys; print(sys.version_info.major)\"" python-shell-interpreter))
+   0 1))
+
+(defun ab-python-find-executables (options)
+  "Find available executables from OPTIONS."
+  (delq nil (mapcar 'executable-find options)))
+
+(defcustom ab-python-python2-hooks nil
+  "Hook for when Python 2 is activated."
+  :group 'ab-python
+  :type 'hook)
+
+(defcustom ab-python-python3-hooks nil
+  "Hook for when Python 3 is activated."
+  :group 'ab-python
+  :type 'hook)
+
+(defun ab-python-activate-python2 ()
+  "Hook run when entering python2 environment."
+  (message "Activating Python 2 toolset.")
+  (set-variable 'python-shell-interpreter (first (ab-python-find-executables '("ipython" "python"))))
+  (unless python-shell-interpreter (warn "No Python executable found!"))
   (set-variable 'traad-environment-root "traad")
   (set-variable 'traad-environment-virtualenv '("virtualenv"))
-  (setup-pyflakes-executable "flake8"))
+  (ab-python-setup-pyflakes-executable "flake8"))
 
-(defun activate-python3 ()
-  (interactive)
-  (setq python-shell-interpreter "ipython3"
-        ;jedi:server-command (list "python3" jedi:server-script)
-        )
-  ;; (set-variable 'traad-server-program '("/usr/local/bin/traad3"))
+(add-hook
+ 'ab-python-python2-hooks
+ 'ab-python-activate-python2)
+
+(defun ab-python-activate-python3 ()
+  "Hook run when entering python3 environment."
+  (message "Activating Python 3 toolset.")
+  (set-variable 'python-shell-interpreter (first (ab-python-find-executables '("ipython3" "python3"))))
+  (unless python-shell-interpreter (warn "No Python executable found!"))
   (set-variable 'traad-environment-root "traad3")
   (set-variable 'traad-environment-virtualenv '("pyvenv-3.4"))
-  (setup-pyflakes-executable "flake8-3"))
+  (ab-python-setup-pyflakes-executable "flake8-3"))
 
-(defun ab-python-ipython-setup ()
-  (setq
-   python-shell-interpreter-args ""
-   python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-   python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-   python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
-   python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
-   python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+(add-hook
+ 'ab-python-python3-hooks
+ 'ab-python-activate-python3)
 
-(defun setup-pyflakes-executable (executable-name)
+;; (defun ab-python-ipython-setup ()
+;;   (setq
+;;    python-shell-interpreter-args ""
+;;    python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;    python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;    python-shell-completion-setup-code
+;;    "from IPython.core.completerlib import module_completion"
+;;    python-shell-completion-module-string-code
+;;    "';'.join(module_completion('''%s'''))\n"
+;;    python-shell-completion-string-code
+;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
+(defun ab-python-setup-pyflakes-executable (executable-name)
   (if (executable-find executable-name)
       (setq flycheck-python-pyflakes-executable executable-name)
     (warn "No python flake8 executable found. Flycheck will be disabled for Python!")))
 
-(defun proper-python-electic-indent ()
-  "The default electric-indent behavior for Python is
-stupid. This does the right thing."
-  (interactive)
-  (newline)
-  (indent-according-to-mode))
+;; (defun proper-python-electic-indent ()
+;;   "The default electric-indent behavior for Python is
+;; stupid. This does the right thing."
+;;   (interactive)
+;;   (newline)
+;;   (indent-according-to-mode))
 
-(defun python-intelligent-fold ()
-  (interactive)
-  (save-excursion
-    (beginning-of-buffer)
-    (while (re-search-forward "^ *def " nil t)
-      (let ((match-pos (match-beginning 0)))
-        (save-excursion
-          (goto-char match-pos)
-          (yafolding-hide-element))))))
+;; (defun python-intelligent-fold ()
+;;   (interactive)
+;;   (save-excursion
+;;     (beginning-of-buffer)
+;;     (while (re-search-forward "^ *def " nil t)
+;;       (let ((match-pos (match-beginning 0)))
+;;         (save-excursion
+;;           (goto-char match-pos)
+;;           (yafolding-hide-element))))))
 
-(defun python-hook ()
+(defun ab-python-hook ()
   (show-paren-mode 1)
-  (electric-indent-local-mode -1)
-  (local-set-key (kbd "RET") 'proper-python-electic-indent)
+  ;; (electric-indent-local-mode -1)
+  ;; (local-set-key (kbd "RET") 'proper-python-electic-indent)
 
   ;; We're using flycheck-pyflakes
   (--map (add-to-list 'flycheck-disabled-checkers it)
-         '(python-flake8 python-pylint ycmd))
-
-  ; (jedi:setup)
-  ;(hs-minor-mode)
-  ; (python-intelligent-fold)
-  )
+         '(python-flake8 python-pylint ycmd)))
 
 (defcustom
   ab-python-patterns
   '("\\.py" "wscript" "SConstruct" "SConsign")
   "File patterns that get put into Python mode.")
 
+;; (use-package python
+;;   :init
+;;   (progn
+;;     (dolist (pattern ab-python-patterns)
+;;       (add-to-list 'auto-mode-alist `(,pattern . python-mode)))
+
+;;     (activate-python3))
+
+;;   :config
+;;   (progn
+;;     (use-package f :ensure t)
+;;     (use-package jedi :ensure t)
+;;     (use-package flycheck-pyflakes :ensure t)
+;;     (use-package python-pep8 :ensure t)
+;;     (use-package python-pylint :ensure t)
+;;     (setq python-indent-offset 4)
+;;     (add-hook 'python-mode-hook 'python-hook)
+;;     (ab-python-ipython-setup)))
+
 (use-package python
   :init
   (progn
     (dolist (pattern ab-python-patterns)
       (add-to-list 'auto-mode-alist `(,pattern . python-mode)))
-
-    (activate-python3))
+    (setq python-indent-offset 4)
+    (run-hooks 'ab-python-python3-hooks))
 
   :config
   (progn
-    (use-package f :ensure t)
-    (use-package jedi :ensure t)
-    (use-package flycheck-pyflakes :ensure t)
-    (use-package python-pep8 :ensure t)
-    (use-package python-pylint :ensure t)
-    (setq python-indent-offset 4)
-    (add-hook 'python-mode-hook 'python-hook)
-    (ab-python-ipython-setup)))
+    (add-hook 'python-mode-hook 'ab-python-hook)))
+
+(use-package flycheck-pyflakes :ensure t)
+(use-package jedi :ensure t)
+
+(defun ab-python-pyvenv-hook ()
+  "Hook for when we change virtual environments."
+
+  ;; Do this so that we're sure to pick up the venv's interpreter.
+  (set-variable 'python-shell-interpreter (executable-find "python"))
+
+  (pyvenv-restart-python)
+
+  ;; Activate the right toolset based on the detected major version.
+  (if (string-equal "3" (ab-python-major-version))
+      (run-hooks 'ab-python-python3-hooks)
+    (run-hooks 'ab-python-python2-hooks)))
+
+(use-package pyvenv
+  :ensure t
+  :init
+  (add-hook 'pyvenv-post-activate-hooks
+            'ab-python-pyvenv-hook))
 
 (provide 'ab-python)
+
+;;; ab-python.el ends here
