95e67902282d62b5ccbf095d15c3c541efc60df7
Austin Bingham <austin.bingham@gmail.com>
2015-01-18 14:53:11 +0100

    Therapy now runs tests reasonably well.

--- elisp/therapy.el
+++ elisp/therapy.el
@@ -7,6 +7,8 @@
 
 ;;; Code:
 
+(require 'dash)
+(require 'f)
 (require 'python)
 
 (defgroup therapy nil
@@ -23,6 +25,9 @@
   "Hook for when Python 3 is activated."
   :group 'therapy
   :type 'hook)
+
+(defvar therapy-test-command "python -m nose"
+  "The command used to execute tests from the project root.")
 
 (defun therapy-interpreter-changed ()
   "Call this when the Python interpreter is changed.
@@ -42,6 +47,57 @@ major version."
          (results (shell-command-to-string command)))
     ;; The output has the major version in the firt character.
     (substring results 0 1)))
+
+(defun therapy-find-project-root (start-dir)
+  "Find the likely Python project root for START-DIR.
+
+Since there's no strict definition of a Python project, this
+makes a few educated guesses.  It first looks for a directory
+containing setup.py.  If it finds one, it assumes that this is
+the root.  Otherwise, it looks for the first parent directory
+that doesn't contain __init__.py which, if found, is the project
+root.
+
+This may get expanded and/or modified in the future."
+  (or (therapy--find-setup-py start-dir)
+      (therapy--find-no-init start-dir)))
+
+;; Testing tools
+
+(defun therapy-run-all-tests ()
+  "Find all unit-tests for the current package and run them."
+  (interactive)
+  (let* ((buffer-dir (f-dirname (buffer-file-name)))
+         (root-dir (therapy-find-project-root buffer-dir)))
+    (unless root-dir (error "No project root detected!"))
+    (let ((default-directory (file-name-as-directory root-dir)))
+      (compile therapy-test-command))))
+
+;; Utility/infrastructure
+
+(defun therapy--parent-dirs (dir)
+  "Find all parent directories of DIR.
+
+Returns a list of directories.  If DIR is not nil then the first
+entry in the list is DIR."
+  (if dir
+    (cons dir (therapy--parent-dirs (f-dirname dir)))))
+
+(defun therapy--find-setup-py (dir)
+  "Search DIR and its parents for setup.py.
+
+Returns the first directory containing setup.py, or nil."
+  (-first
+   (lambda (dir) (f-exists? (f-join dir "setup.py")))
+   (therapy--parent-dirs dir)))
+
+(defun therapy--find-no-init (dir)
+  "Search DIR and its parents for directories without __init__.py.
+
+Returns the first directory not containing __init__.py, or nil."
+  (-first
+   (lambda (dir) (not (f-exists? (f-join dir "__init__.py"))))
+   (therapy--parent-dirs dir)))
 
 (provide 'therapy)
 
