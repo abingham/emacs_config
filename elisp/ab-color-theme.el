3a2500f521829439d90361c27b53fd00f1f8763e
Austin Bingham <austin.bingham@gmail.com>
2015-03-17 11:55:11 +0100

    Now using leuven theme.
    This theme is awesomely good.

--- elisp/ab-color-theme.el
+++ elisp/ab-color-theme.el
@@ -2,10 +2,9 @@
   :ensure t
   :init
   (progn
-    ;; (use-package color-theme-solarized)
-    ;; (use-package zenburn-theme)
     (add-to-list 'custom-theme-load-path (file-name-directory load-file-name))
-    (load-theme 'zorkon t)))
+    (use-package leuven-theme)
+    (load-theme 'leuven t)))
 
 (provide 'ab-color-theme)
 ;; ab-color-theme.el ends here
